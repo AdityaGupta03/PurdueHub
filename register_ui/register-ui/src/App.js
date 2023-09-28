2954c5ea86a68e3488fdffb4ff3efd2b115ef73c
Leonardo Delgado <89818698+lddelgadhub@users.noreply.github.com>
2023-09-28 12:10:20 -0400

    Begin work on Sign In UI

--- register_ui/register-ui/src/App.js
+++ register_ui/register-ui/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import Register from './Register';
+import Login from './Login';
 import './App.css';
 
 function App() {
+  
   return (
     <main className="App">
-      {/* Self Contained Component To Call From */}
-      <Register /> 
+      {/* Self Contained Component To Call From  <Register /> */}
+       <Login />
     </main>
   );
 }
