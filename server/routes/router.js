1b6faf7837e924c042fc79e56cf73f1c5fad65a3
Aditya Gupta <adgupta1003@gmail.com>
2023-10-01 02:05:42 -0400

    Added routing for verify email api.

--- server/routes/router.js
+++ server/routes/router.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/create_account", accountController.createAccount);
 router.post("/update_username", accountController.updateUsername);
 router.post("/reset_username", resetUsername);
+router.post("/verify_email", accountController.verifyEmail);
 
 module.exports = router;
