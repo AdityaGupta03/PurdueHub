39184085e5fbc296f6d12f4605762af05126016e
Aditya Gupta <adgupta1003@gmail.com>
2023-10-08 16:46:03 -0400

    Setup AWS RDS integration in project.

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,23 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "localhost",
-    port: 5432,
-    database: "purduehub"
-});
+let pool;
+
+if (process.env.NODE_ENV === "local") {
+    pool = new Pool({
+        user: "postgres",
+        password: "postgres",
+        host: "localhost",
+        port: 5432,
+        database: "purduehub"
+    });
+} else if (process.env.NODE_ENV === "aws") {
+    pool = new Pool({
+        user: "postgres",
+        password: "postgres",
+        host: "database-1.cvhxyqtbrscf.us-east-2.rds.amazonaws.com",
+        port: 5432,
+        database: "database-1"
+    });
+}
 
 module.exports = pool;
