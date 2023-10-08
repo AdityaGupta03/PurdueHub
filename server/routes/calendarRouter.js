fc613ca9054477b42d91e8bbb7786f909353d28a
Aditya Gupta <adgupta1003@gmail.com>
2023-10-08 17:55:21 -0400

    Fixed to GET HTTP method.

--- server/routes/calendarRouter.js
+++ server/routes/calendarRouter.js
@@ -3,6 +3,6 @@ const calendarController = require("../controllers/calendarController");
 
 const calendarRouter = express.Router();
 
-// calendarRouter.post("/get_calendar", calendarController.getCalendar);
+calendarRouter.get("/get_calendar", calendarController.getCalendar);
 
 module.exports = calendarRouter;
