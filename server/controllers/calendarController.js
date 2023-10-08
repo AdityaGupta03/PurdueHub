6c3cad0a4bb3eff00bbdfcc4d9d0499d3956b360
Aditya Gupta <adgupta1003@gmail.com>
2023-10-08 18:24:38 -0400

    Created backend apis to edit user calendar.

--- server/controllers/calendarController.js
+++ server/controllers/calendarController.js
@@ -38,6 +38,112 @@ async function getCalendar(req, res) {
     }   
 }
 
+async function updateCalendarEvent(req, res) {
+    console.log("[Info] Updating calendar event for user " + req.body.username);
+    
+    const username = req.body.username;
+    if (!username) {
+        console.error("[Error] Missing username");
+        return res.status(400).json({ error: "Missing username" });
+    }
+
+    const { start, end, title, description, location, organization_id, id } = req.body;
+    if (!start || !end || !title || !description || !location || !organization_id || !id) {
+        console.error("[Error] Missing fields");
+        return res.status(400).json({ error: "Missing fields" });
+    }
+
+    const calendar_event = {
+        id: id,
+        start: start,
+        end: end,
+        title: title,
+        description: description,
+        location: location,
+        organization_id: organization_id,
+    };
+
+    try {
+        await calendarQueries.updateCalendarEventQuery(calendar_event);
+        return res.status(200).json({ message: "Calendar event updated" });
+    } catch (error) {
+        console.error("[Error] " + error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+async function deleteCalendarEvent(req, res) {
+    console.log("[Info] Deleting calendar event for user " + req.body.username);
+
+    const username = req.body.username;
+    if (!username) {
+        console.error("[Error] Missing username");
+        return res.status(400).json({ error: "Missing username" });
+    }
+
+    const id = req.body.id;
+
+    try {
+        await calendarQueries.removeCalendarEventsArrQuery(username, id);
+        permanentlyDeleteCalendarEvent(id);
+        return res.status(200).json({ message: "Calendar event deleted" });
+    } catch (error) {
+        console.error("[Error] " + error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+async function permanentlyDeleteCalendarEvent(id) {
+    console.log("[Info] Checking if calendar event " + id + " is still in use");
+
+    try {
+        let result = await calendarQueries.isCalendarEventInUseQuery(id);
+        if (result === false) {
+            console.log("[Info] Calendar event " + id + " is not in use, deleting permanently");
+            await calendarQueries.deleteCalendarEventQuery(id);
+        }
+    } catch (error) {
+        console.error("[Error] " + error);
+    }
+}
+
+async function addCalendarEvent(req, res) {
+    console.log("[Info] Adding calendar event for user " + req.body.username);
+
+    const username = req.body.username;
+    if (!username) {
+        console.error("[Error] Missing username");
+        return res.status(400).json({ error: "Missing username" });
+    }
+
+    const { start, end, title, description, location, organization_id } = req.body;
+    if (!start || !end || !title || !description || !location || !organization_id) {
+        console.error("[Error] Missing fields");
+        return res.status(400).json({ error: "Missing fields" });
+    }
+
+    const calendar_event = {
+        start: start,
+        end: end,
+        title: title,
+        description: description,
+        location: location,
+        organization_id: organization_id,
+    };
+
+    try {
+        await calendarQueries.addCalendarEventQuery(calendar_event);
+        await calendarQueries.addCalendarEventsArrQuery(username, calendar_event.id);
+        return res.status(200).json({ message: "Calendar event added" });
+    } catch (error) {
+        console.error("[Error] " + error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+}
+
 module.exports = {
     getCalendar,
+    updateCalendarEvent,
+    deleteCalendarEvent,
+    addCalendarEvent,
 };
--- server/routes/calendarRouter.js
+++ server/routes/calendarRouter.js
@@ -4,5 +4,8 @@ const calendarController = require("../controllers/calendarController");
 const calendarRouter = express.Router();
 
 calendarRouter.get("/get_calendar", calendarController.getCalendar);
+calendarRouter.post("/update_calendar_event", calendarController.updateCalendarEvent);
+calendarRouter.post("/delete_calendar_event", calendarController.deleteCalendarEvent);
+calendarRouter.post("/add_calendar_event", calendarController.addCalendarEvent);
 
 module.exports = calendarRouter;
