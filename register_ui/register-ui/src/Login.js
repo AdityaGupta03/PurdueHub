879f71a935058bb92b7de14733bc047dd587ec99
Leonardo Delgado <89818698+lddelgadhub@users.noreply.github.com>
2023-10-12 19:47:39 -0400

    created basic calendar

--- register_ui/register-ui/src/Login.js
+++ register_ui/register-ui/src/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useRef, useState, useEffect} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+
 export const Login = () => {
     const navigate = useNavigate();
 
