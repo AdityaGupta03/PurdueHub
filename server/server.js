f9b7dd1bedc81d0ad51709c2e16ca4f9823a5084
Aditya Gupta <adgupta1003@gmail.com>
2023-10-11 20:17:51 -0400

    Added routing for register and connected to backend.
    Signed-off-by: Aditya Gupta <adgupta1003@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/router");
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: "http://localhost:3000",
+}));
 
 app.use(bodyParser.json());
 
