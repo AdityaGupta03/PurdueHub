9ab8cb5f59a56b94db57559908b563a712a7071d
Aditya Gupta <adgupta1003@gmail.com>
2023-11-01 15:50:21 -0400

    Added search users integration.
    Signed-off-by: Aditya Gupta <adgupta1003@gmail.com>

--- register_ui/register-ui/src/UsernameLookup.js
+++ register_ui/register-ui/src/UsernameLookup.js
@@ -1,66 +1,74 @@
 import React from 'react'
-import { useState } from "react";
-import { useParams, useNavigate, Link } from 'react-router-dom';
-import {FaSearch} from "react-icons/fa"
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaSearch } from "react-icons/fa"
 import "./styles.css"
-import TestProfile from './TestProfile';
-var data = require("./MOCKDATA.json");
 
 function UsernameLookup() {
-    const [value, setValue] = useState("");
-    const navigate = useNavigate();
+  const [ value, setValue ] = useState("");
+  const [ data, setData ] = useState([]);
 
-    const onChange = (event) => {
-      setValue(event.target.value);
-    };
-  
-    const onSearch = (searchTerm) => {
-        setValue(searchTerm);
-        // backend?
-        navigate(`/testprofile/${searchTerm}`);
-    };
-  
-    return (
-      <div className="full">
-        <h1 className='searchTitle'>Username Lookup</h1>
-  
-        <div className="search-container">
-          <div className="search-inner">
-          <FaSearch id="search-icon"/>
-            <input 
-                className='searchInput'
-                type="text" 
-                value={value} 
-                onChange={onChange} 
-                placeholder='Type to search...' />
-          </div>
-          <div className="dropdown">
-            {data
-              .filter((item) => {
-                const searchTerm = value.toLowerCase();
-                const user = item.username.toLowerCase();
-  
-                return (
-                  searchTerm &&
-                  user.startsWith(searchTerm) &&
-                  user !== searchTerm
-                );
-              })
-              .slice(0, 10)
-              .map((item) => (
-                <div
-                  onClick={() => onSearch(item.username)}
-                  className="dropdown-row"
-                  key={item.username}
-                >
-                  {item.username}
-                </div>
-              ))}
-          </div>
+  const navigate = useNavigate();
+
+  const onChange = async (event) => {
+    setValue(event.target.value);
+    console.log("Searching on " + event.target.value);
+    try {
+      let res = await fetch('http://localhost:5000/api/search_users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ "search": event.target.value }),
+      });
+
+      const data = await res.json();
+      console.log(data.users);
+      if (res.status === 200) {
+        setData(data.users);
+      } else {
+        setData([]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log("Data is " + data);
+  };
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    navigate(`/viewprofile/${searchTerm}`);
+  };
+
+  return (
+    <div className="full">
+      <h1 className='searchTitle'>Username Lookup</h1>
+
+      <div className="search-container">
+        <div className="search-inner">
+          <FaSearch id="search-icon" />
+          <input
+            className='searchInput'
+            type="text"
+            value={value}
+            onChange={onChange}
+            placeholder='Type to search...' />
+        </div>
+        <div className="dropdown">
+          {data.map((item) => (
+              <div
+                onClick={() => onSearch(item)}
+                className="dropdown-row"
+                key={item}
+              >
+                {item}
+              </div>
+            ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default UsernameLookup
