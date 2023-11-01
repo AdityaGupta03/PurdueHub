753a0952ddb9bf85e08ccdf832e6abb7a387696f
Leonardo Delgado <89818698+lddelgadhub@users.noreply.github.com>
2023-11-01 18:53:46 -0400

    added remove logic to professional and callouts, fixed facebook removal issue

--- register_ui/register-ui/src/EventsInterestedPage.js
+++ register_ui/register-ui/src/EventsInterestedPage.js
@@ -6,17 +6,17 @@ import works from './fireworks-shape.jpg' // temp picture
 function EventsInterestedPage() {
 
     const navigate = useNavigate();
-    const[profDevData, setProfDevData] = useState([]); // pull data of professional development tagged events
-    const[generalClubCallData, setGeneralCluCallbData] = useState([]); // pull data of club clalout tagged events
-    const[notifyProfDev, setNotifyProfDev] = useState(false); // pull notification setting if they toggled this option
-    const[notifyGeneralCallout, setNotifyGeneralCallout] = useState(false); // pull notification setting if they toggled this option
-    const[isUpcoming, setIsUpcoming] = useState(true); // boolean to check if there upcoming events
 
-    const [isInterested, setIsInterested] = useState(true);
+    const[notifyProfDev, setNotifyProfDev] = useState(true); // pull notification setting if they toggled this option
+    const[notifyGeneralCallout, setNotifyGeneralCallout] = useState(true); // pull notification setting if they toggled this option
+    const[isUpcoming, setIsUpcoming] = useState(true); // boolean to check if there upcoming events that a user followed for
 
-    const [viewingEvent, setViewingEvent] = useState(false);
 
+    const [isInterested, setIsInterested] = useState(true); // general boolean used for removing events / adding back them back
+    const [viewingEvent, setViewingEvent] = useState(false); 
+    
     const [index, setIndex] = useState(0);
+    const [checkTag, setCheckTag] = useState('');
     const [wantsToRemove, setWantsToRemove] = useState(false);
     const [eventName, setEventName] = useState('X'); // specific event information
     const [eventDescription, setEventDescription] = useState('X'); // specific event information
@@ -76,6 +76,8 @@ function EventsInterestedPage() {
     ];
 
     const [allEvents, setAllEvents] = useState(generalData);
+    const [profDevEvents, setProfDevEvents] = useState(profData) // pull data of professional development tagged events
+    const [calloutEvents, setCalloutEvents] = useState(callData) // pull data of club clalout tagged events
 
     const followEvent = () => {
         if(isInterested) {
@@ -95,31 +97,52 @@ function EventsInterestedPage() {
         console.log('');
     }, [viewingEvent])
 
-    const viewEventPageClick = (name, description, indexOf) => {
-        
-        if(viewingEvent) {
+    const viewEventPageClick = (name, description, indexOf, tag) => {
+        if(viewingEvent) {  
             // if no longer viewing the page of the event...
             setEventName('');
             setEventDescription('');
+
             if(wantsToRemove) {
                 // remove event from list
-
-                const events = allEvents;
-                events.splice(index, 1);
-                if(events.length === 0) {
-                    setIsUpcoming(false);
+                if(checkTag === "general") {
+                    console.log(checkTag);
+                    const events = allEvents;
+                    events.splice(index, 1);
+                    if(events.length === 0) {
+                        setIsUpcoming(false); // this means there exists no events anymore since a user removed them all
+                    }
+                    setAllEvents(events);   
                 }
+                else if (checkTag === "prof") {
+                    console.log(checkTag);
+                    const profEvents = profDevEvents;
+                    profEvents.splice(index, 1);
+                    if(profEvents.length === 0) {
+                        setNotifyProfDev(false); // this means there exists no events anymore since a user removed them all
+                    }
+                    setProfDevEvents(profEvents);   
 
-                setAllEvents(events);   
-
+                } else {
+                    console.log(checkTag);
+                    const callEvents = calloutEvents;
+                    callEvents.splice(index, 1);
+                    if(callEvents.length === 0) {
+                        setNotifyGeneralCallout(false); // this means there exists no events anymore since a user removed them all
+                    }
+                    setCalloutEvents(callEvents);   
+                }
                 setIsInterested(true);
             }
+            setWantsToRemove(false);
+            setCheckTag("x");
             setViewingEvent(false);
         }
         else {
             // if viewing the page of the event...
             setEventName(name);
             setIndex(indexOf);
+            setCheckTag(tag);
             setEventDescription(description);
             setViewingEvent(true);
         }
@@ -130,7 +153,7 @@ function EventsInterestedPage() {
         {viewingEvent ? (
                 <div className='interestedWhole'>
                     <div className='containbtn'>
-                        <button className='actualbtn' onClick={() => viewEventPageClick('X', 'X')}>Back</button>
+                        <button className='actualbtn' onClick={() => viewEventPageClick('X', 'X', 0, "X")}>Back</button>
                     </div>
 
                     <div className='intro'>
@@ -170,19 +193,21 @@ function EventsInterestedPage() {
             <h2 style={{textDecoration: 'underline'}}>Professional Development:</h2>
                 {notifyProfDev ? (
                      <div>
-                        {profData.map((item, index) => {
+                        {profDevEvents.map((item, index) => {
                              return (
+                                <Link className="clubEventLinks"style={{textDecorationLine: 'none'}} onClick={() => viewEventPageClick(item.eventName, item.description, index, "prof")}>
                                 <div key={index} className='event'>
                                     <h3>Event Name:</h3>
                                     <p className='wrapText'>{item.eventName}</p>
                                     <h3>Event Description: </h3>
                                     <p className='wrapText'>{item.description}</p>
                                 </div>
+                                </Link>
                                 )    
                             })}
                      </div>
                 ) : (
-                    <p>You Don't Have Notifications For This Enabled</p>
+                    <p>Nothing Coming Up...</p>
                 )}
         </div>
 
@@ -190,28 +215,30 @@ function EventsInterestedPage() {
             <h2 style={{textDecoration: 'underline'}}>Club Callout:</h2>
                 {notifyGeneralCallout ? (
                      <div>
-                        {callData.map((item, index) => {
+                        {calloutEvents.map((item, index) => {
                              return (
+                                <Link className="clubEventLinks"style={{textDecorationLine: 'none'}} onClick={() => viewEventPageClick(item.eventName, item.description, index, "callout")}>
                                 <div key={index} className='event'>
                                     <h3>Event Name:</h3>
                                     <p className='wrapText'>{item.eventName}</p>
                                     <h3>Event Description: </h3>
                                     <p className='wrapText'>{item.description}</p>
                                 </div>
+                                </Link>
                                 )    
                             })}
                      </div>
                 ) : (
-                    <p>You Don't Have Notifications For This Enabled</p>
+                    <p>Nothing Coming Up...</p>
                 )}
         </div>
         <div className='eventCtn'>
-            <h2 style={{textDecoration: 'underline'}}>General Events</h2>
+            <h2 style={{textDecoration: 'underline'}}>General Events:</h2>
                 {isUpcoming ? (
                      <div>
                         {allEvents.map((item, index) => {
                              return (
-                                <Link className="clubEventLinks"style={{textDecorationLine: 'none'}} onClick={() => viewEventPageClick(item.eventName, item.description, index)}>
+                                <Link className="clubEventLinks"style={{textDecorationLine: 'none'}} onClick={() => viewEventPageClick(item.eventName, item.description, index, "general")}>
                                 <div key={index} className='event'>
                                     <h3>Event Name:</h3>
                                     <p className='wrapText'>{item.eventName}</p>
